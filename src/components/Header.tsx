fb10db18e8110f6b4e960917708c5bfba97c5b42
Viktor <28753370+kirillovmr@users.noreply.github.com>
2024-07-28 13:03:04 -0600

    upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,8 +14,8 @@ export default function Header() {
       <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
         <div className="flex flex-col items-center justify-between gap-1">
           <div>
-            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl center">
-              Stat Viewer v1.0
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-2xl center">
+              DeFiTracker StatViewer <span className="text-sm">v1.1.0</span>
             </h1>
           </div>
           <p className="text-xs">{fileName}</p>
