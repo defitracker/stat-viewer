a43eb00f66929cf8b7bc3d592298ce17c2608e39
Viktor <28753370+kirillovmr@users.noreply.github.com>
2024-07-31 11:49:25 -0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header";
-import FileSelect from "./components/FileSelect";
-import { useMyStore } from "./store";
-import CallStack from "./components/CallStack";
-import Renderer from "./components/Renderer";
-import { useShallow } from "zustand/react/shallow";
-import S3FileSelect from "./components/S3FileSelect";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function App() {
-  const { fileData } = useMyStore(
-    useShallow((state) => ({
-      fileData: state.fileData,
-    }))
-  );
-
   return (
-    <>
-      <section className="bg-white">
-        <Header />
-        <div className="container px-6 py-2 flex flex-col gap-2 mx-auto">
-          {!fileData && (
-            <>
-              <FileSelect />
-              <span className="relative flex justify-center my-4">
-                <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
-
-                <span className="relative z-10 bg-white px-6 text-gray-700">
-                  Or load data from S3
-                </span>
-              </span>
-              <S3FileSelect />
-            </>
-          )}
-
-          {fileData && (
-            <>
-              <CallStack />
-              <Renderer />
-            </>
-          )}
-        </div>
-      </section>
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
